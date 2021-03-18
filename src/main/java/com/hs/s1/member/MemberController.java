dd4ffa91d3dc9b2fa6dc8b6d6972597d10e963cc
HyungsooLim <hyungsoolim0@gmail.com>
2021-03-18 15:49:50 +0900

    spring_lecture_2_210318

--- src/main/java/com/hs/s1/member/MemberController.java
+++ src/main/java/com/hs/s1/member/MemberController.java
@@ -2,6 +2,7 @@ package com.hs.s1.member;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,23 +10,56 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MemberController {
 
+	@Autowired
+	private MemberService memberService;
+
+	// memberJoin
+	// URL : /member/memberJoin GET
+	@RequestMapping(value = "/member/memberJoin", method = RequestMethod.GET)
+	public String memberJoin() {
+
+		return "member/memberJoin";
+	}
+
+	// memberJoin2 -> 회원가입 진행
+	// URL : /member/memberJoin POST
+	@RequestMapping(value = "/member/memberJoin", method = RequestMethod.POST)
+	public void memberJoin2(String id, String pw, String name, String phone, String email) throws Exception {
+		MemberDTO memberDTO = new MemberDTO();
+		memberDTO.setId(id);
+		memberDTO.setPw(pw);
+		memberDTO.setName(name);
+		memberDTO.setPhone(phone);
+		memberDTO.setEmail(email);
+		int result = memberService.memberJoin(memberDTO);
+		if (result != 0) {
+			System.out.println("회원가입 성공");
+		} else {
+			System.out.println("회원가입 실패");
+		}
+	}
+
 	// memberJoin print
 	// URL : /member/memberLogin
 	@RequestMapping(value = "/member/memberLogin", method = RequestMethod.GET)
 	public String memberLogin() {
+//		System.out.println(name);
+//		System.out.println(age);
 		System.out.println("Member Login");
-
 		return "member/memberLogin";
 	}
 
 	// memberLogin2 print
 	@RequestMapping(value = "/member/memberLogin", method = RequestMethod.POST)
-	public void memberLogin2(HttpServletRequest request) {
+	public void memberLogin2(HttpServletRequest request) throws Exception {
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
-		System.out.println("--- Login Success ---");
-		System.out.println("ID : "+id);
-		System.out.println("PW : "+pw);
+		MemberDTO memberDTO = new MemberDTO();
+		memberDTO.setId(id);
+		memberDTO.setPw(pw);
+		memberDTO = memberService.memberLogin(memberDTO);
+
+		System.out.println(memberDTO);
 
 	}// === memberLogin2 method END ===
 }
---
+++ src/main/java/com/hs/s1/util/DBConnector.java
@@ -0,0 +1,16 @@
+package com.hs.s1.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnector {
+
+	public void getConnection() throws Exception {
+		String user="user01";
+		String password="user01";
+		String url="jdbc:oracle:thin:@127.0.0.1:1521:xe";
+		String driver="oracle.jdbc.driver.OracleDriver";
+		Class.forName(driver);
+		Connection con = DriverManager.getConnection(url, user, password);
+	}
+}
