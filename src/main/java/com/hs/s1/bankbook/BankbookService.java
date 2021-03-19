e80cae3c980f1716a37fbbc1c48de46a2ca68ae4
HyungsooLim <hyungsoolim0@gmail.com>
2021-03-19 16:09:54 +0900

    bankbook_progress_1_210319
    finished bankbookSelect

--- src/main/java/com/hs/s1/bankbook/BankbookService.java
+++ src/main/java/com/hs/s1/bankbook/BankbookService.java
@@ -1,5 +1,21 @@
 package com.hs.s1.bankbook;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BankbookService {
+	@Autowired
+	private BankbookDAO bankbookDAO;
+
+	public List<BankbookDTO> getList() throws Exception {
+		return bankbookDAO.getList();
+	}
+
+	public BankbookDTO getSelect(BankbookDTO bankbookDTO) throws Exception {
+		return bankbookDAO.getSelect(bankbookDTO);
+	}
 
 }
